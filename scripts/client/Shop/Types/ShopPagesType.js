26fc9ad41e067db89bded19746224d9dc51107f3
Cake <78360666+CakeA@users.noreply.github.com>
2021-02-04 15:42:29 +0100

    Added form styles and shop templates

--- scripts/client/Shop/Types/ShopPagesType.js
+++ scripts/client/Shop/Types/ShopPagesType.js
@@ -2,7 +2,9 @@ Client.shop.types.pages = function() {
     this.$element = $(
         '<div class="shop-pages">' +
             '<div class="shop-pages-left">' +
-                '<div class="shop-pages-search"></div>' +
+                '<div class="shop-pages-search">' +
+                    '<input type="text" placeholder="Search...">' +
+                '</div>' +
                 
                 '<div class="shop-pages-list dialog-container"></div>' +
             '</div>' +
