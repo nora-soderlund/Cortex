d270cc5bd0d7dbeeaae0fd79e29e197082af13a3
Cake <78360666+CakeA@users.noreply.github.com>
2021-02-06 14:17:29 +0100

    Cleared up furniture icons

--- scripts/client/Furnitures/Icons/FurnitureIcon.js
+++ scripts/client/Furnitures/Icons/FurnitureIcon.js
@@ -7,18 +7,21 @@ Client.furnitures.icon = async function(id) {
 
     const visualization = new Client.furnitures.visualization(assets.manifest.visualization.visualizationData, 1);
 
-    const data = new Client.furnitures.assets(assets.manifest.assets.assets.asset);
+    const assetsData = new Client.furnitures.assets(assets.manifest.assets.assets.asset);
 
     const layers = [];
 
-    let maxWidth = 0, maxHeight = 0, maxLeft = 0, maxTop = 0;
+    const data = { maxWidth: 0, maxHeight: 0, maxLeft: 0, maxTop: 0 };
 
     for(let index = 0; index < visualization.layerCount; index++) {
         const layer = Client.utils.charCode(parseInt(index));
 
         const name = furniture.id + "_icon_" + layer;
 
-        const sprite = data.getAsset(name);
+        const sprite = assetsData.getAsset(name);
+
+        if(sprite == undefined)
+            continue;
         
         const layerData = {
             image: await Client.assets.getSprite(library, name),
@@ -28,25 +31,25 @@ Client.furnitures.icon = async function(id) {
 
         layers.push(layerData);
 
-        if((layerData.left + layerData.image.width) > maxWidth)
-            maxWidth = (layerData.left + layerData.image.width);
+        if((layerData.left + layerData.image.width) > data.maxWidth)
+            data.maxWidth = (layerData.left + layerData.image.width);
 
-        if((layerData.top + layerData.image.height) > maxHeight)
-            maxHeight = (layerData.top + layerData.image.height);
+        if((layerData.top + layerData.image.height) > data.maxHeight)
+            data.maxHeight = (layerData.top + layerData.image.height);
 
-        if(layerData.top > maxTop)
-            maxTop = layerData.top;
+        if(layerData.top > data.maxTop)
+            data.maxTop = layerData.top;
             
-        if(layerData.left > maxLeft)
-            maxLeft = layerData.left;
+        if(layerData.left > data.maxLeft)
+            data.maxLeft = layerData.left;
     }
 
-    const $canvas = $('<canvas width="' + (maxWidth - maxLeft) + '" height="' + (maxHeight - maxTop) + '"></div>');
+    const $canvas = $('<canvas width="' + (data.maxWidth - data.maxLeft) + '" height="' + (data.maxHeight - data.maxTop) + '"></div>');
 
     const context = $canvas[0].getContext("2d");
 
     for(let index in layers)
-        context.drawImage(layers[index].image, maxLeft + -layers[index].left, maxTop + -layers[index].top);
+        context.drawImage(layers[index].image, data.maxLeft + -layers[index].left, data.maxTop + -layers[index].top);
 
     return $canvas[0];
 };
