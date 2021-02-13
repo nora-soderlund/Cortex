f648d286717a2ca9508cc4508b38db42c13719e9
Cake <78360666+CakeA@users.noreply.github.com>
2021-02-13 13:01:03 +0100

    Fixed z-index depth calculations

--- scripts/client/Rooms/Items/RoomItemEntity.js
+++ scripts/client/Rooms/Items/RoomItemEntity.js
@@ -96,7 +96,7 @@ Client.rooms.items.entity = function(parent, name) {
 
         this.offset = [ Math.floor(-(position.row * 32) + (position.column * 32) - 64), Math.floor((position.column * 16) + (position.row * 16) - (position.depth * 32)) ];
 
-        this.index = (position.row * 750) + (position.column * 750) + (position.depth * 1000) + index;
+        this.index = (position.row * 1000) + (position.column * 1000) + (position.depth * 100) + index;
     };
 
     this.setCoordinates = function(row, column, depth, index = 0) {
@@ -108,7 +108,7 @@ Client.rooms.items.entity = function(parent, name) {
 
         //this.index = ((row + column + Math.trunc(index / 1000)) * 100) + depth;
 
-        this.index = (row * 750) + (column * 750) + (depth * 1000) + index;
+        this.index = (row * 1000) + (column * 1000) + (depth * 100) + index;
     };
     
     this.process = function(frame) {
