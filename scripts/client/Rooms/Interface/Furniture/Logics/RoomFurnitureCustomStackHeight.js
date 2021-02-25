11a1588b49d4aaede056bb096ac42426fa0c4f5c
Cake <78360666+CakeA@users.noreply.github.com>
2021-02-26 00:11:01 +0100

    Changed render priorities and added custom stack height dialogs

--- scripts/client/Rooms/Interface/Furniture/Logics/RoomFurnitureCustomStackHeight.js
+++ scripts/client/Rooms/Interface/Furniture/Logics/RoomFurnitureCustomStackHeight.js
@@ -1,3 +1,26 @@
 Client.rooms.interface.furniture.logics.furniture_custom_stack_height = function(entity) {
+    if(entity.dialog != undefined) {
+        entity.dialog.show();
 
+        return;
+    }
+
+    entity.dialog = new Client.dialogs.default({
+        title: entity.furniture.furniture.title,
+        
+        size: {
+            width: 240,
+            height: 120
+        },
+
+        offset: {
+            type: "center"
+        }
+    });
+
+    entity.dialog.events.destroy.push(function() {
+        entity.dialog = undefined;
+    });
+
+    entity.dialog.show();
 };
