85010eb6cbb8919b8bf80b6e454f89331d915372
Cake <78360666+CakeA@users.noreply.github.com>
2021-03-04 06:35:17 +0100

    Added experimental depth handling edit

--- scripts/client/Rooms/Items/RoomItemEntity.js
+++ scripts/client/Rooms/Items/RoomItemEntity.js
@@ -91,7 +91,7 @@ Client.rooms.items.entity = function(parent, name) {
         }
     };
 
-    this.setPosition = function(position, index = 0) {
+    /*this.setPosition = function(position, index = 0) {
         this.data.position = position;
 
         this.offset = [ Math.floor(-(position.row * 32) + (position.column * 32) - 64), Math.floor((position.column * 16) + (position.row * 16) - (position.depth * 32)) ];
@@ -112,6 +112,21 @@ Client.rooms.items.entity = function(parent, name) {
         //this.index = ((row + column + Math.trunc(index / 1000)) * 500) + depth;
 
         this.index = (row * 1000) + (column * 1000) + (depth * 100) + index;
+
+        if(parent.door != undefined && parent.door.row == Math.round(row) && parent.door.column == Math.round(column))
+            this.index = -11000 + index;
+    };*/
+
+    this.setPosition = function(position, index = 0) {
+        this.setCoordinates(position.row, position.column, position.depth, index);
+    };
+
+    this.setCoordinates = function(row, column, depth, index = 0) {
+        this.data.position = { row, column, depth };
+
+        this.offset = [ Math.floor(-(row * 32) + (column * 32) - 64), Math.floor((column * 16) + (row * 16) - (depth * 32)) ];
+
+        this.index = (row * 1000) + (column * 1000) + depth + index;
 
         if(parent.door != undefined && parent.door.row == Math.round(row) && parent.door.column == Math.round(column))
             this.index = -11000 + index;
