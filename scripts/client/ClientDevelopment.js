f65033c2482fbb494ee3d6fe90d9a058e62afc0a
Cake <78360666+CakeA@users.noreply.github.com>
2021-03-05 11:57:19 +0100

    Added development dialog

--- scripts/client/ClientDevelopment.js
+++ scripts/client/ClientDevelopment.js
@@ -47,3 +47,71 @@ Client.development.frames = new function() {
         Client.rooms.interface.frameLimit += direction;
     });
 };
+
+Client.loader.ready(function() {
+    Client.development.furni = new function() {
+        const entity = new Client.dialogs.default({
+            title: "Loading",
+            
+            size: {
+                width: 340,
+                height: 80
+            },
+
+            offset: {
+                type: "absolute",
+
+                left: "12.5%",
+                top: "12.5%"
+            }
+        });
+
+        entity.events.create.push(function() {
+        });
+
+        entity.set = async function(furniture) {
+            entity.$content.html("");
+
+            const data = await Client.furnitures.get(furniture.data.furniture);
+
+            const depth = await Client.socket.messages.sendCall({ OnFurnitureDepthRequest: data.id }, "OnFurnitureDepthRequest");
+            
+            $(
+                '<div class="dialog-property">' +
+                    '<p>' +
+                        '<b>Furniture Depth</b>' +
+                        '<span>How high the furniture is in units!</span>' + 
+                    '</p>' +
+                    
+                    '<div class="input-pen">' +
+                        '<input class="furniture-depth" type="text" placeholder="Enter a furniture depth..." value="' + depth + '">' +
+                    '</div>' + 
+                '</div>'
+            ).appendTo(entity.$content).find(".furniture-depth").on("change", async function() {
+                await Client.socket.messages.sendCall({ Temp_DevFurniUpdate: { id: data.id, depth: parseFloat($(this).val()) } }, "Temp_DevFurniUpdate");
+            });
+
+            console.log(data);
+
+            Client.development.furni.unpause();
+
+            entity.setTitle('HabboFurnitures/' + data.line + '/' + data.id);
+        };
+
+        entity.show();
+        
+        entity.pause();
+
+        return entity;
+    };
+
+    Client.rooms.interface.cursor.events.click.push(function(entity) {
+        if(entity == undefined || entity.entity.name != "furniture") {
+            Client.development.furni.pause();
+            
+            return;
+        }
+        
+        Client.development.furni.set(entity.entity);
+    });
+});
