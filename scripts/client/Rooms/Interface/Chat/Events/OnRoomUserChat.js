37ee6c786852d6a857f19ff4f69409671f452432
Cake <78360666+CakeA@users.noreply.github.com>
2021-03-11 20:26:59 +0100

    Added figure talking animation when chatting

--- scripts/client/Rooms/Interface/Chat/Events/OnRoomUserChat.js
+++ scripts/client/Rooms/Interface/Chat/Events/OnRoomUserChat.js
@@ -1,4 +1,4 @@
-Client.socket.messages.register("OnRoomUserChat", function(data) {
+Client.socket.messages.register("OnRoomUserChat", async function(data) {
     const user = Client.rooms.interface.users[data.id];
                 
     const center = Client.rooms.interface.entity.center;
@@ -6,4 +6,12 @@ Client.socket.messages.register("OnRoomUserChat", function(data) {
     const offset = user.getOffset();
 
     Client.rooms.interface.chat.addMessage("bot_a", "**" + user.data.name + ":** " + data.message, center + offset[0] + 64);
+
+    await user.figure.setAction("Talk");
+
+    setTimeout(async function() {
+        await user.figure.removeAction("Talk");
+
+        user.figure.render();
+    }, data.message.length * 100);
 });
